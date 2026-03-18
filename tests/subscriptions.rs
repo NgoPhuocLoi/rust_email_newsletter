5ddec3cc69116d9129684f65936b5cb6142c3be9
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-03-18 18:34:29 +0700

    Prepare expected test cases for POST /subscriptions endpoint

---
+++ tests/subscriptions.rs
@@ -0,0 +1,31 @@
+use actix_web::{App, http::header::ContentType, test};
+use rust_email_newsletter::subscribe;
+
+
+#[actix_web::test]
+async fn subscribe_return_200_for_a_valid_form_data() {
+    let app = test::init_service(App::new().service(subscribe)).await;
+
+    let req = test::TestRequest::post().uri("/subscriptions")
+        .insert_header(ContentType::form_url_encoded())
+        .set_payload("username=LoiNgo&email=nploi@axonactive.com")
+        .to_request();
+    
+    let resp = test::call_service(&app, req).await;
+    
+    assert!(resp.status().is_success());
+}
+
+#[actix_web::test]
+async fn subscribe_return_400_when_data_is_missing() {
+    let app = test::init_service(App::new().service(subscribe)).await;
+
+    let req = test::TestRequest::post().uri("/subscriptions")
+        .insert_header(ContentType::form_url_encoded())
+        .set_payload("username=LoiNgo")
+        .to_request();
+    
+    let resp = test::call_service(&app, req).await;
+    
+    assert!(resp.status().is_client_error());
+}
