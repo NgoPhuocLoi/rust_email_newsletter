6de6265e6361bacf27845dce3cbd038585dce9e8
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-04-05 21:12:06 +0700

    Enhance logging setup with tracing and bunyan formatter

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use env_logger::Env;
 use rust_email_newsletter::{configuration::get_configuration, startup::run};
 use sqlx::PgPool;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // Output the formatted spans to stdout.
+        std::io::stdout,
+    );
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    // `set_global_default` can be used by applications to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
     let config = get_configuration().expect("Fail to load config");
     let pool = PgPool::connect(&config.db.connection_string())
         .await
