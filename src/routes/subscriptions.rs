b538740774eeaa6f16b3ddfea3ca7d4ec07d8240
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-04-12 14:15:29 +0700

    Cleanup instrumentation code by using  procedural macro

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,8 +4,7 @@ use actix_web::{
 };
 use chrono::Utc;
 use serde::Deserialize;
-use sqlx::PgPool;
-use tracing::Instrument;
+use sqlx::{PgPool, postgres::PgQueryResult};
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -14,38 +13,40 @@ pub struct SubscriptionFormData {
     email: String,
 }
 
+#[tracing::instrument(
+    name = "Adding subscription",
+    skip(form, pool),
+    fields(
+        request_id = %Uuid::new_v4(),
+        email = %form.email,
+        username = %form.username
+    )
+)]
 #[post("subscriptions")]
 pub async fn subscribe(form: Form<SubscriptionFormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
+    match insert_subscription(&form, &pool).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().body("failed"),
+    }
+}
 
-    let request_span = tracing::info_span!("Adding new subscription.", %request_id, subscription_email = %form.email, subscription_username = %form.username);
-
-    let _request_span_guard = request_span.enter();
-
-    let query_span = tracing::info_span!("Executing DB query to save subscription");
-
-    let saved_result = sqlx::query(
+#[tracing::instrument(name = "Inserting subscription", skip(form, pool))]
+async fn insert_subscription(
+    form: &Form<SubscriptionFormData>,
+    pool: &PgPool,
+) -> Result<PgQueryResult, sqlx::Error> {
+    let result = sqlx::query(
         "INSERT INTO subscription (email, username, subscribed_at) VALUES ($1, $2, $3)",
     )
     .bind(&form.email)
     .bind(&form.username)
     .bind(Utc::now())
-    .execute(pool.get_ref())
-    .instrument(query_span)
-    .await;
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute the query: {:?}", e);
+        e
+    })?;
 
-    match saved_result {
-        Ok(_) => {
-            tracing::info!("[Request ID: {}] New subscription created!", request_id);
-            HttpResponse::Ok().finish()
-        }
-        Err(e) => {
-            tracing::error!(
-                "[Request ID: {}] Failed to create subscription: {}",
-                request_id,
-                e
-            );
-            HttpResponse::InternalServerError().body("failed")
-        }
-    }
+    Ok(result)
 }
