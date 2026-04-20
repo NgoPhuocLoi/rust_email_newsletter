0b9f09a13beb96f3ef94dfd43071422d1f2d99ef
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2026-04-20 19:55:26 +0700

    Adding a simple username validation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -5,6 +5,7 @@ use actix_web::{
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::{PgPool, postgres::PgQueryResult};
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(Deserialize)]
 pub struct SubscriptionFormData {
@@ -22,6 +23,11 @@ pub struct SubscriptionFormData {
 )]
 #[post("subscriptions")]
 pub async fn subscribe(form: Form<SubscriptionFormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    if !is_valid_name(&form.username) {
+        tracing::info!("Invlaid username: {}", &form.username);
+        return HttpResponse::BadRequest().finish();
+    }
+
     match insert_subscription(&form, &pool).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().body("failed"),
@@ -47,4 +53,26 @@ async fn insert_subscription(
     })?;
 
     Ok(result)
+}
+
+pub fn is_valid_name(name: &str) -> bool {
+    const MAXIMUM_NAME_LENGTH: usize = 256;
+    const FORBBIDEN_CHARS: [char; 9] = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+
+    // Should not be empty
+    if name.trim().is_empty() {
+        return false;
+    }
+
+    // Should be shorter than MAXIMUM_NAME_LENGTH
+    if name.graphemes(true).count() > MAXIMUM_NAME_LENGTH {
+        return false;
+    }
+
+    // Should not contain any forbbiden characters
+    if name.chars().any(|c| FORBBIDEN_CHARS.contains(&c)) {
+        return false;
+    }
+
+    true
 }
